3cf6415f8d7013181fd53de57790bdf3f6e55856
krolmiki2011 <mikolajziolkowski504@gmail.com>
2025-07-19 16:49:05 +0200

    Update

--- atomix/profiledef.sh
+++ atomix/profiledef.sh
@@ -15,14 +15,17 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
-  ["/etc/gshadow"]="0:0:400"
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
-  ["/etc/polkit-1/rules.d"]="0:0:750"
-  ["/etc/sudoers.d"]="0:0:750"
+  ["/root/install.sh"]="0:0:755"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/bin/__aldm-deploy"]="0:0:755"
+  ["/usr/bin/aldm-bootstrap"]="0:0:755"
+  ["/usr/bin/aldm-deploy"]="0:0:755"
+  ["/usr/bin/aldm-chroot"]="0:0:755"
+  ["/usr/bin/aldm-release"]="0:0:755"
 )
